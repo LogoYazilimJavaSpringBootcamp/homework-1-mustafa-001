5fcef4b7a1cfd247f9472d5a8eac6c3ddf79fc40
Mustafa Mutlu <mustafaalimutlu@gmail.com>
2022-06-15 12:52:47 +0300

    Add homework requirements.

--- src/main/java/com/fatura/App.java
+++ src/main/java/com/fatura/App.java
@@ -8,13 +8,11 @@ import com.fatura.entities.Customer;
 import com.fatura.entities.Invoice;
 
 import java.math.BigDecimal;
+import java.time.Month;
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Hello world!
- */
 public class App {
     public static void main(String[] args) {
         var customers = new ArrayList<Customer>(List.of(
@@ -29,13 +27,76 @@ public class App {
         ));
         var invoices = new ArrayList<Invoice>(List.of(
                 new Invoice(ZonedDateTime.now().minusDays(4), new BigDecimal(10000), companies.get(0)),
-                new Invoice(ZonedDateTime.now().minusDays(5),  new BigDecimal(1000), companies.get(1)),
-                new Invoice(ZonedDateTime.now().minusDays(6),  new BigDecimal(100), companies.get(1))
+                new Invoice(ZonedDateTime.now().minusDays(5), new BigDecimal(1000), companies.get(1)),
+                new Invoice(ZonedDateTime.now().minusDays(6), new BigDecimal(100), companies.get(1))
         ));
+
+        customers.get(1).getInvoices().add(invoices.get(1));
 
         var customerDb = new CustomerDatabase(customers);
         var companiesDb = new CompanyDatabase(companies);
         var invoicesDb = new InvoiceDatabase(invoices);
-        customerDb.getAll().forEach(System.out::println);
+
+        // Just to add two additional Customer's with letter C in their names.
+        customerDb.add("Mehmet Ceylan", ZonedDateTime.now());
+        customerDb.add("Hatice Kırlangıç", ZonedDateTime.now());
+
+        homeworkRequirements(customerDb, companiesDb, invoicesDb);
+    }
+
+    public static void homeworkRequirements(CustomerDatabase customerDatabase, CompanyDatabase companyDatabase, InvoiceDatabase invoiceDatabase) {
+
+        System.out.println("İçerisinde C harfi bulunan müsteriler:");
+        customerDatabase.filter(it -> it.getName().contains("C") || it.getName().contains("c")).forEach(it -> System.out.println(it.getName()));
+
+        List<Customer> customersInJune = customerDatabase.filter(it -> it.getSignupDate().getMonth().equals(Month.JUNE) && it.getSignupDate().getYear() == 2022);
+        BigDecimal totalAmountByUsersInJune = customersInJune
+                .stream()
+                .flatMap(invoices -> invoices.getInvoices().stream())
+                .map(Invoice::getAmount)
+                .reduce(new BigDecimal(0), BigDecimal::add);
+        System.out.println("----------------------------");
+        System.out.print("Haziran ayında kayıt olan müsterilerin faturalarının toplam tutarı:\t");
+        System.out.println(totalAmountByUsersInJune.toString());
+
+        System.out.println("----------------------------");
+        System.out.println("Sistemdeki tüm faturalar:");
+        invoiceDatabase.getAll().forEach(System.out::println);
+
+        System.out.println("----------------------------");
+        System.out.println("Sistemdeki 1500TL üstündeki faturalar:");
+        invoiceDatabase.filter(it -> it.getAmount().intValue() > 1500).forEach(System.out::println);
+
+        System.out.println("----------------------------");
+        System.out.println("Sistemdeki 1500TL üstündeki faturaların ortalaması:");
+        List<Invoice> invoicesBiggerThan1500 = invoiceDatabase.filter(it -> it.getAmount().intValue() > 1500);
+        System.out.println(invoicesBiggerThan1500
+                .stream()
+                .map(Invoice::getAmount)
+                .reduce(new BigDecimal(0), BigDecimal::add)
+                .floatValue() / invoicesBiggerThan1500.size()
+        );
+
+        System.out.println("----------------------------");
+        System.out.println("Sistemdeki 500TL üstündeki faturalara sahip müşterilerin isimleri:");
+        customerDatabase
+                .filter(customer -> customer
+                        .getInvoices()
+                        .stream()
+                        .anyMatch(it -> it.getAmount().intValue() > 500)
+                )
+                .forEach(it -> System.out.println(it.getName()));
+
+        System.out.println("----------------------------");
+        System.out.println("Haziran ayı faturalarının ortalaması 750TL altı olan firmaların sektörleri:");
+        List<Invoice> invoices;
+        for (var company : companyDatabase.getAll()) {
+            invoices = invoiceDatabase.filter(it -> it.getCompany().equals(company));
+            float average = invoices.stream().map(Invoice::getAmount).reduce(new BigDecimal(0), BigDecimal::add).floatValue() / invoices.size();
+            if (average < 750) {
+                System.out.println(company.getField());
+            }
+        }
+
     }
 }
